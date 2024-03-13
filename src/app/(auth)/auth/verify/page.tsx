c255e2ef9cc705540ec8c11afe59f6d1a51d7b1a
Abenezer <abenezerberhanu40@gmail.com>
2024-03-13 14:52:30 +0300

    finish verfy page

--- src/app/(auth)/auth/verify/page.tsx
+++ src/app/(auth)/auth/verify/page.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import React from "react";
-import Link from "next/link";
 import FormHeader from "@/components/uiComponent/FormHeader";
 import { Button } from "@/components/ui/button";
 import OtpInput from "@/components/uiComponent/OtpInput";
@@ -9,14 +7,14 @@ function SigninForm() {
   return (
     <section className="bg-white min-h-screen flex flex-col items-center justify-center relative">
       <Image
-        src={"/assets/authTrasparentSvg.png"}
+        src={"/assets/authSVG1.png"}
         alt="car svg"
         width={500}
         height={1000}
         className="absolute top-4 right-4 max-w-[300px] z-0"
       />
       <Image
-        src={"/assets/authTrasparentSvg1.png"}
+        src={"/assets/authSVG2.png"}
         alt="car svg"
         width={500}
         height={1000}
@@ -29,9 +27,30 @@ function SigninForm() {
           hide={true}
         />
         <div className="flex flex-col gap-3">
-          <h1 className="text-2xl font-bold text-zinc-600">
-            Verify your account 💬
-          </h1>
+          <span className="flex items-center gap-1">
+            <h1 className="text-2xl font-bold text-zinc-600 ">
+              Verify your account
+            </h1>
+            {/* svg showing warning so will come over it */}
+            {/* <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-message-circle-more"
+            >
+              <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
+              <path d="M8 12h.01" />
+              <path d="M12 12h.01" />
+              <path d="M16 12h.01" />
+            </svg> */}
+          </span>
+
           <p className="text-gray_text">
             We sent a verification code to your mobile. Enter the code from the
             mobile in the field below.
