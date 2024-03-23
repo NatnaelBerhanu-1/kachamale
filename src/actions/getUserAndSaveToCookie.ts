52833aaaa3c2d18e55ca2cfa8e26543f140bf34b
Abenezer <abenezerberhanu40@gmail.com>
2024-03-23 22:29:06 +0300

    add feature to retrive data from DB by using user phone and save to cookie

---
+++ src/actions/getUserAndSaveToCookie.ts
@@ -0,0 +1,43 @@
+"use server";
+import connectDB from "@/lib/config";
+import UserModel from "@/model/userRegistrationModel";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+interface userInfoType {
+  _id: string;
+  fullName: string;
+  phoneNumber: string;
+  isActive: boolean;
+}
+
+export const getUserAndSaveToCookie = async ({
+  phoneNumber,
+}: {
+  phoneNumber: string;
+}) => {
+  try {
+    await connectDB();
+    //get the user by hes number
+    const user = (await UserModel.findOne({
+      phoneNumber,
+    }).lean()) as userInfoType;
+    //generate token that contains user id, name, phoneNumber
+    const userInfo = {
+      id: user._id,
+      fullName: user.fullName,
+      phoneNumber: user.phoneNumber,
+    };
+    const accessToken = jwt.sign(userInfo, process.env.ACCESS_TOKEN_KEY!);
+
+    //add this token to cookie authentication header
+
+    cookies().set({
+      name: "Et_car_Token",
+      value: accessToken,
+      httpOnly: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
