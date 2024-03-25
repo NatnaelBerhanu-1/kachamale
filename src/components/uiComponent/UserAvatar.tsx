313c278e200a2c4edf3b99e38861cdc82951c68f
Abenezer <abenezerberhanu40@gmail.com>
2024-03-25 13:01:44 +0300

    add conditional statment for header user avatar is not user info is exist the avatar is gonna be log in button

--- src/components/uiComponent/UserAvatar.tsx
+++ src/components/uiComponent/UserAvatar.tsx
@@ -1,10 +1,25 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-function UserAvatar() {
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { verifyToken } from "@/actions/verifyToken";
+async function UserAvatar() {
+  const user = await verifyToken();
   return (
-    <Avatar>
-      <AvatarImage src="https://github.com/shadcn.png" />
-      <AvatarFallback className="animate-pulse"></AvatarFallback>
-    </Avatar>
+    <>
+      {user ? (
+        <Avatar title={user.fullName + "profile"} className="cursor-pointer">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback className="animate-pulse"></AvatarFallback>
+        </Avatar>
+      ) : (
+        <Button
+          asChild
+          className="hover:ring-1 ring-white hover:bg-transparent bg-transparent font-medium"
+        >
+          <Link href={"/signin"}>Log in</Link>
+        </Button>
+      )}
+    </>
   );
 }
 
