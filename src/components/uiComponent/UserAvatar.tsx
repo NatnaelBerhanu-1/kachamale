220e09f2fa32522c7fe512e838a7d2aa4eb0ae72
Abenezer <abenezerberhanu40@gmail.com>
2024-03-27 00:04:07 +0300

    add dropdown to header avatar

--- src/components/uiComponent/UserAvatar.tsx
+++ src/components/uiComponent/UserAvatar.tsx
@@ -2,19 +2,43 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { verifyToken } from "@/actions/verifyToken";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut } from "lucide-react";
 
 async function UserAvatar() {
   const user = await verifyToken();
   return (
     <>
       {user ? (
-        <Avatar
-          title={user.fullName + " " + "profile"}
-          className="cursor-pointer"
-        >
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback className="animate-pulse"></AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            {" "}
+            <Avatar
+              title={user.fullName + " " + "profile"}
+              className="cursor-pointer"
+            >
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback className="animate-pulse"></AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="z-40">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem className="my-2 font-semibold bg-slate-100 flex items-center">
+              <LogOut className="size-4" /> Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <Button
           asChild
