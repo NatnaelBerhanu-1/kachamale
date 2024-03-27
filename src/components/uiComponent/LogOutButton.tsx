22eb30cc10e8a32a19c54a587a6d30bfede3e56f
Abenezer <abenezerberhanu40@gmail.com>
2024-03-27 15:39:58 +0300

    fix user navigation after signup

--- src/components/uiComponent/LogOutButton.tsx
+++ src/components/uiComponent/LogOutButton.tsx
@@ -3,15 +3,16 @@ import { signUserOut } from "@/actions/logoutUser";
 import { LogOut } from "lucide-react";
 import React from "react";
 import { useEffect, useState } from "react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function LogOutButton() {
   const [value, setValue] = useState(false);
+  const { push } = useRouter();
 
   useEffect(() => {
     if (value) {
       localStorage.removeItem("et_uid");
-      redirect("/signin");
+      push("/signin");
     }
   }, [value]);
 
